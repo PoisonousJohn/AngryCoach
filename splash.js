cb17c808aea9a49986419ebc56bec0eb138e2ead
JohnPoison <johnny.j.poison@gmail.com>
2016-11-20 22:32:56 +0300

    removed debug file

--- splash.js
+++
@@ -1,8 +0,0 @@
-import fn from '../';
-const mobisplash = require('mobisplash');
-
-mobisplash('logo.svg', {platform: 'android', draw9patch: false}).then(() => {
-    console.log("generated");
-});
-
-
